cd5780848521b7dcd59ddc8aa246ffddd887b7b9
fnajer <lexactr@gmail.com>
2018-08-10 14:10:56 +0300

    Check and format cords for MarkerInput and Map

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -26,18 +26,40 @@ class App extends Component {
   handleInputSumbit = (event) => {
     event.preventDefault();
     const listMarkers = this.state.listMarkers;
+    let cords;
+
+    try {
+      cords = this.checkCords(this.state.cords);
+    } catch (error) {
+      return alert('Coordinates not right. Try again.');
+    }
 
     const newMarker = {
       id: listMarkers[listMarkers.length - 1].id + 1,
-      coordinates: this.state.cords.split(' '),
+      coordinates: cords,
     };
-    newMarker[0] = +newMarker[0];
-    newMarker[1] = +newMarker[1];
 
     this.setState(prevState => ({
       cords: '',
       listMarkers: [...prevState.listMarkers, newMarker],
     }));
+  }
+
+  checkCords(cords) {
+    const arrCords = cords.split(' ');
+
+    if (arrCords.length > 2 || 
+        arrCords.length < 2 ||
+        isNaN(arrCords[0]) ||
+        isNaN(arrCords[1]) ||
+        arrCords[0] === '' ||
+        arrCords[1] === ''
+    ) throw new Error();
+
+    arrCords[0] = +arrCords[0];
+    arrCords[1] = +arrCords[1];
+
+    return arrCords;
   }
 
   render() {

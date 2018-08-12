09752f0c840cd454031bdcdac22042e77c049c9a
fnajer <lexactr@gmail.com>
2018-08-12 22:06:29 +0300

    Refactoring MarkerInput component

--- src/components/MarkerInput/index.jsx
+++ src/components/MarkerInput/index.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
-const MarkerInput = ({ cords, handleInputChange, handleInputSumbit }) => {
-  return (
-    <form onSubmit={handleInputSumbit}>
-      <input
-        value={cords}
-        onChange={handleInputChange}
-        className="w-100"
-        type="text" 
-        placeholder="New point of routes" 
-        style={{ border: '2px solid #000', padding: '5px 10px' }} />
-    </form>
-  );
-};
+class MarkerInput extends PureComponent {
+  render() {
+    const { cords, handleInputChange, handleInputSumbit } = this.props;
+    return (
+      <form onSubmit={handleInputSumbit}>
+        <input
+          value={cords}
+          onChange={handleInputChange}
+          className="w-100"
+          type="text" 
+          placeholder="New point of routes" 
+          style={{ border: '2px solid #000', padding: '5px 10px' }} />
+      </form>
+    );
+  }
+}
 
 export default MarkerInput;
